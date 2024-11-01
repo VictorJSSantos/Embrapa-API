9a177bf6d113fb8a50e5e4c1f47aa8be642b4faa
VictorJSSantos <victormvll@gmail.com>
2024-11-01 10:48:19 -0300

    Realizando ajustes finos na função de coleta de dados.

--- app/transform.py
+++ app/transform.py
@@ -40,46 +40,24 @@ class Transform(Requisition):
     def format_data(self, response):
         soup = BeautifulSoup(response, "html.parser")
         soup = soup.find("thead").parent
-        # df = pd.DataFrame()
-        headers = ["category"]
+        headers = [th.text.strip() for th in soup.find_all("th")]
         # Inicializa uma lista para armazenar os resultados
-        tds_em_trs = []
+        data_by_row_to_list = []
         df = pd.DataFrame()
-        # Inicializando a lista de categorias
-        category = []
 
         # Itera sobre as linhas da tabela
         for tr in soup.find_all("tr"):
             # Para cada linha, extrai o texto de cada célula (td)
             tds = tr.find_all("td")
 
-            # Verifica se a linha é um tb_item e extrai a categoria
-            if "tb_item" in (td.get("class", [None])[0] for td in tds):
-                tb_item = tds[0].text.strip()  # Remove espaços e quebras de linha
-                category.append(tb_item)
-
-            # Adiciona o resultado como um array de 3 elementos (ou quantos td's houver)
-            tds_formatados = [
+            # Adiciona o resultado como um array de elementos
+            row_value_list = [
                 td.text.strip().replace("\n", "") for td in tds
             ]  # Formata cada td
 
-            tds_em_trs.append(tds_formatados)
+            data_by_row_to_list.append(row_value_list)
 
-        df = pd.DataFrame(tds_em_trs)
-
-        if len(headers) == 2:
-            tds_em_trs.append(
-                tds_formatados + [category[-1] if category else None]
-            )  # Adiciona a categoria
-            # Converte a lista para um DataFrame
-            df = pd.DataFrame(
-                tds_em_trs, columns=["Produto", "Quantidade (L.)", "item_class"]
-            )
-
-        if len(headers) == 3:
-            df = pd.DataFrame(
-                tds_em_trs, columns=["País", "Quantidade (L.)", "Valor (US$)"]
-            )
+        df = pd.DataFrame(data_by_row_to_list, columns=headers)
 
         return df.to_dict(orient="dict")
 
@@ -110,5 +88,5 @@ class Transform(Requisition):
 
         for subarea in SubModel:
             response = await self.get_all_data(area=area, subarea=subarea.name)
-            data[f"{subarea.name}"] = response
+            data[f"{subarea.value}"] = response
         return data
