2a26837e55b6284617539f5d9c24009fecb76902
VictorJSSantos <victormvll@gmail.com>
2024-11-05 09:08:42 -0300

    Ajustando arquivos para manter versão final.

--- app/utils.py
+++ app/utils.py
@@ -6,7 +6,6 @@ import os
 
 
 from app.models import *
-from response_data import *
 
 
 SECRET_KEY = os.environ["SECRET_KEY"]
@@ -46,29 +45,3 @@ def transform_input_into_string(year, area, subarea=None):
         return str(year), str(area), str(subarea)
     else:
         return str(year), str(area), None
-
-
-def offline_fallback_get_data(year, area, subarea=None):
-    dict_of_files = {
-        "Produção": "response_production.json",
-        "Processamento": "response_processing.json",
-        "Importação": "response_imports.json",
-        "Exportação": "response_imports.json",
-        "Comercialização": "response_sales.json",
-    }
-    file_path = f"response_data/{dict_of_files.get(area)}"
-    try:
-        # Carregar JSON do arquivo
-        with open(file_path, "r") as f:
-            json_data = json.load(f)
-
-        # Verificar e acessar subarea e ano, se especificados
-        if subarea:
-            return json_data.get(subarea, {}).get(
-                str(year), f"Dados para '{subarea}' no ano {year} não encontrados."
-            )
-        else:
-            return json_data.get(str(year), f"Dados para o ano {year} não encontrados.")
-
-    except (FileNotFoundError, KeyError) as e:
-        return {"error": f"Erro ao carregar dados para '{area}': {str(e)}"}
